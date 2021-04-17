64fbe55327aa4ff019b3d00bfc4d22d08c46203a
Sachin Sunny <sachin@Sachins-MacBook-Pro.local>
2021-04-17 10:52:23 -0400

    cors issue

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const apiMocker = require('connect-api-mocker');
-
+const cors = require('cors');
 const port = 5000;
 const app = express();
  
+app.use(cors);
 app.use('/api', apiMocker('api'));
  
 console.log(`Mock API Server is up and running at: http://localhost:${port}`);
